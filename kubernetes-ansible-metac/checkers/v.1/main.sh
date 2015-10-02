59cfba1149c9abba444076c26917e05a2744ec2f
Sergii Chebotkin <mor.sergei@gmail.com>
2015-10-02 15:37:56 +0300

    Code refactoring / modularised

--- kubernetes-ansible-metac/checkers/v.1/main.sh
+++ kubernetes-ansible-metac/checkers/v.1/main.sh
@@ -5,10 +5,20 @@
 cfgDir=`pwd`"/"
 cfgFile="default.cfg"
 CONFIGURATION=$cfgDir$cfgFile
+DEBUG=3
+
+fPrint() 
+{
+	case $DEBUG in
+		1);;
+		2);;
+		3) ((fPrt++)) ; echo "[ $fPrt ] :  ${@}" ;;
+	esac
+}
 
 getConfig()
 {
-	echo "Configure from: "${@}
+	fPrint "Configure from: ${@}"
 	source ${@}
 }
 
@@ -18,55 +28,40 @@ getParam()
 		do 
 			case $args in
 				-c=* | --configure=*)
-					CONFIGURATION="${args#*=}"
-					#echo $CONFIGURATION
-					((step++))
-					shift
+				CONFIGURATION="${args#*=}"
+				((step++))
+				shift
     			;;
+				-m=* | --module=*)
+				MOD_INS="${args#*=}.mod"
+				shift
+			;;
+
     			*) 
 					DEFAULT=true
 				;;
 			esac
 		done
-	if [ $DEFAULT ]; then  ((step++));echo "Your parameter $step are wrong"; fi
+	[[ $DEFAULT ]] &&  ((step++));echo "Your parameter $step are wrong" 
 }
 
-getPortStatus()
-{	
-	local url=$1
-	local port=$2
-	echo `curl -s -o /dev/null -w "%{http_code}" $url:$port`
-}
-
-function checkUAnswer()
+function modStarter()
 {
-	local answer=$1; echo "Server answer: $answer"
-	local oarr_len=${#OK_CODES[@]}
-	local earr_len=${#OK_CODES[@]}
-	local ok_code=0
-	local err_code=0
+	local fName=${@}
+	fPrint "modStarter: $fName"
 
-	for (( i=0; i<${oarr_len}; i++ ));
-		do
-  			#echo OK ${OK_CODES[$i]}
-  			if [ $answer -eq ${OK_CODES[$i]} ]; then ((ok_code++)); fi
-		done
-
-        for (( i=0; i<${earr_len}; i++ ));
-                do
-                        #echo ERR ${ERR_CODES[$i]}
-                        if [ $answer -eq ${ERR_CODES[$i]} ]; then ((err_code++)); fi
-                done
-	
-	if [ $ok_code -eq 1 ]; then return 0
-	elif [ $err_code -eq 1 ]; then return 1
-	else return 3
+	if [ -n $fName ]  
+		then 
+			fPrint "Module attaced: $MODULES$fName" 
+			source $MODULES$fName
+		else 
+			fPrint "No any module has found" 
+			exit 3
 	fi
 }
 
 getParam ${@}
 getConfig ${CONFIGURATION}
-checkUAnswer $(getPortStatus $URL $PORT)
-echo $?
+modStarter $MOD_INS
 
-exit 0 
+
